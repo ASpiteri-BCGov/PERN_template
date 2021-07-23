11fdc87505bc002ee70eff32d41bc52aa9eb0d88
adam spiteri <adam.spiteri@gov.bc.ca>
2021-07-23 15:03:47 -0700

    started on client folder

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// var fs = require('fs');
+var fs = require('fs');
 
 // const directories = ['./client','./openshift','./server']
 
@@ -12,15 +12,45 @@
 //     fs.appendFile('client/public/index.html', data, function (err) {
 //         if (err) throw err;
 //         console.log('Saved!');
-//       });    
+//       });
 //   });
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-   
-  readline.question('Who are you?', name => {
-    console.log(`Hey there ${name}!`);
-    readline.close();
-  });
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// readline.question('What is the name of your project?', (name) => {
+//   console.log(`name`, name);
+//   readline.close();
+// });
+
+//Create gitignore file for the frontend ie "client"
+// let consoleSuccess = '\x1b[32m';
+
+// fs.readFile('FileContents/client/.gitignore', function (err, data) {
+//   fs.appendFile('client/.gitignore', data, function (err) {
+//     if (err) throw err;
+//     console.log(consoleSuccess, 'Success client/.gitignore created!');
+//   });
+// });
+
+// fs.readFile('FileContents/client/package-lock.json', function (err, data) {
+//   fs.appendFile('client/package-lock.json', data, function (err) {
+//     if (err) throw err;
+//     console.log(consoleSuccess, 'Success client/package-lock.json created!');
+//   });
+// });
+// fs.readFile('FileContents/client/package.json', function (err, data) {
+//   fs.appendFile('client/package.json', data, function (err) {
+//     if (err) throw err;
+//     console.log(consoleSuccess, 'Success client/package.json created!');
+//   });
+// });
+// fs.readFile('FileContents/client/README.md', function (err, data) {
+//   fs.appendFile('client/README.md', data, function (err) {
+//     if (err) throw err;
+//     console.log(consoleSuccess, 'Success client/README.md created!');
+//     console.log('\x1b[0m', ''); //Reset colors
+//   });
+// });
